f0c1399cfebb7d7a0cd7b2a58561df5d58c95c87
xiangcaiee <61265097+bail-mingsy@users.noreply.github.com>
2024-04-16 21:23:15 +0800

    Update

---
+++ adBlock.js
@@ -0,0 +1,14 @@
+// ScriptName   套壳影视adblock
+// ScriptURL    https://raw.githubusercontent.com/bail-mingsy/QX/main/adBlock.js
+
+
+hostname = *pangolin-sdk-toutiao*, mi.gdt.qq.com, open.e.kuaishou.com
+
+# > Disciplineddriver去开屏和首页广告首次打开可能不成功再次打开即可去除
+^https?:\/\/(?:sf3-fe-tos\.pglstatp-toutiao\.com|api-access\.pangolin-sdk-toutiao[0-9]*\.com|log-api\.pangolin-sdk-toutiao-b\.com|v9-be-pack\.pglstatp-toutiao\.com)\/ url reject-dict
+
+# > 小柿子去开屏广告
+^https?:\/\/(?:open\.e\.kuaishou\.com\/rest\/e\/v[23]\/open\/|mi\.gdt\.qq\.com\/gdt_mview\.fcg) url reject-dict 
+
+# > 小柿子去首页notice
+^http:\/\/162\.209\.174\.253:12346\/xsz\.php\/v6\/top_notice\? url reject-200
